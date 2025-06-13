28e9a04d74f1b77c9e98c1e72d70bdabc17bb801
Benadette Mokandu <benadettemokandu@gmail.com>
2025-06-14 00:38:05 +0300

    oky dokey

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+    
+}
+console.log(saturdayFun("bathe my dog"));
+console.log(saturdayFun("roller-skate"));
+
+const mondayWork = function(activity2="go to the office") {
+    return(`This Monday, I will ${activity2}.`);
+}
+console.log(mondayWork("go to the office"));
+console.log(mondayWork("work from home"));
+
+
+function wrapAdjective(flair="*"){
+    return function okCreativity(special="a hard worker"){
+        return (`You are ${flair}${special}${flair}!`);
+    }
+}
+
+console.log(wrapAdjective("!!"));
